01594ddd111fd457069b4cf5fb80bca3bb1c9707
Denis Anastasev <anastasevdenis@gmail.com>
2022-11-01 22:34:36 +0300

    add requirements.txt

--- apollodshop/dataloads/cities.py
+++ apollodshop/dataloads/cities.py
@@ -1,30 +1,22 @@
 from typing import List
 
-import pandas as pd
-
 from aiographql.client import GraphQLResponse
 from tqdm import tqdm
 
-def get_cities_info(data: GraphQLResponse) -> List:
 
+def get_cities_info(data: GraphQLResponse) -> List:
     cities_raw = data['dshopMain']['cities']
 
-    cities_raw_list = []
     delivery_points_raw_list = []
 
     for el in tqdm(range(len(cities_raw))):
 
-        cities_raw_list.append({
-
-            'city_id': cities_raw[el]['id'],
-            'name': cities_raw[el]['name'],
-            'min_free_delivery_price_courier': cities_raw[el]['minFreeDeliveryPriceCourier'],
-            'min_free_delivery_price_dp': cities_raw[el]['minFreeDeliveryPriceDp']
-        })
-
         for el2 in range(len(cities_raw[el]['deliveryPoints'])):
             delivery_points_raw_list.append({
                 'city_id': cities_raw[el]['id'],
+                'name': cities_raw[el]['name'],
+                'min_free_delivery_price_courier': cities_raw[el]['minFreeDeliveryPriceCourier'],
+                'min_free_delivery_price_dp': cities_raw[el]['minFreeDeliveryPriceDp'],
                 'id': cities_raw[el]['deliveryPoints'][el2]['id'],
                 'address': cities_raw[el]['deliveryPoints'][el2]['address'],
                 'delivery_date': cities_raw[el]['deliveryPoints'][el2]['deliveryDate'],
@@ -35,9 +27,4 @@ def get_cities_info(data: GraphQLResponse) -> List:
                 'time_to': cities_raw[el]['deliveryPoints'][el2]['timeTo']
             })
 
-    cities = pd.DataFrame(cities_raw_list)
-    deliv_points = pd.DataFrame(delivery_points_raw_list)
-
-    delivery_points = deliv_points.merge(cities, how='left', left_on='city_id', right_on='city_id')
-
-    return delivery_points.values.tolist()
+    return delivery_points_raw_list
